13be887d5a78dac5c3298ce0f32a32d7343cf5df
LucaZecchetti <lucazecchetti@gmail.com>
2023-12-06 23:57:08 +0100

    changed some text

--- src/events.js
+++ src/events.js
@@ -1,13 +1,13 @@
 window.addEventListener('scroll', function() {
+    //add to the navbar a class when it goes over another specific class and removes it when it goes out
     var navbar = document.querySelector('.navbar');
     var otherClass = document.querySelector('.content-container');
     var navbarPosition = navbar.getBoundingClientRect();
     var otherClassPosition = otherClass.getBoundingClientRect();
-    console.log(navbarPosition.bottom+" >= "+otherClassPosition.top,navbarPosition.bottom >= otherClassPosition.top)
     if (navbarPosition.bottom >= otherClassPosition.top) {
       navbar.classList.add('scrolled-past');
     } else {
       navbar.classList.remove('scrolled-past');
     }
-  });
+});
   
